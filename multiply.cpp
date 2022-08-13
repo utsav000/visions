22b02a6b3b4317e34865664de2fceb8d9d1fedce
utsav000 <utsavgupta8770@gmail.com>
2022-08-13 18:47:58 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+int a;
+int b;
+cin>>a>>b;
+cout<<"the product of the two number is"<<a*b<<endl;
+}
+
